ee6c98e3c1b558fbb125301f2d26334f2f2ada2a
rRakarare <rRene333@googlemail.com>
2021-05-05 22:49:55 +0200

    asddsa

--- src/screens/ProjectDetail.js
+++ src/screens/ProjectDetail.js
@@ -1,67 +1,65 @@
-import React, {useState, useEffect} from 'react'
-import { Grid, Segment, Statistic } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Grid, Segment, Statistic, Step, Icon } from "semantic-ui-react";
 import axiosInstance from "../axios/axios";
 
-function ProjectDetail({match}) {
+function ProjectDetail({ match }) {
+  const projectID = match.params.id;
+  const [data, setData] = useState({});
 
-    const projectID = match.params.id
-    const [data, setData] = useState({})
+  const SegmentStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
 
-    const SegmentStyle = {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      };
-
-    const getProjectData = async () => {
-        try {
-            const res = await axiosInstance.get(`projects/${projectID}`)
-            setData(res.data)
-        } catch(err) {
-            return err.message
-        }
+  const getProjectData = async () => {
+    try {
+      const res = await axiosInstance.get(`projects/${projectID}`);
+      setData(res.data);
+    } catch (err) {
+      return err.message;
     }
+  };
 
-    useEffect(() => {
-        getProjectData()
-    }, [])
+  useEffect(() => {
+    getProjectData();
+  }, []);
 
-    return (
-        <Grid style={{ margin: "2rem" }} columns={3}>
-        <Grid.Row stretched>
-          <Grid.Column width={2}></Grid.Column>
-          <Grid.Column width={11}>
-            {data.title}
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Segment
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Statistic size="small">
-                <Statistic.Value>22</Statistic.Value>
-                <Statistic.Label>Aktive Projekte</Statistic.Label>
-              </Statistic>
-            </Segment>
-            <Segment style={SegmentStyle}>
-              <Statistic size="small">
-                <Statistic.Value>22.000 €</Statistic.Value>
-                <Statistic.Label>Auftragssumme</Statistic.Label>
-              </Statistic>
-            </Segment>
-            <Segment style={SegmentStyle}>
-              <Statistic size="small">
-                <Statistic.Value>3</Statistic.Value>
-                <Statistic.Label>Projekte in Aussicht</Statistic.Label>
-              </Statistic>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    )
+  return (
+    <Grid style={{ margin: "2rem" }} columns={3}>
+      <Grid.Row stretched>
+        <Grid.Column width={2}></Grid.Column>
+        <Grid.Column width={11}>{data.title}</Grid.Column>
+        <Grid.Column width={3}>
+          <Step.Group fluid vertical>
+            <Step>
+              <Icon name="signup" />
+              <Step.Content>
+                <Step.Title>Angebot</Step.Title>
+                <Step.Description>Angebot an Kunden</Step.Description>
+              </Step.Content>
+            </Step>
+
+            <Step>
+              <Icon name="play" />
+              <Step.Content>
+                <Step.Title>Bearbeitung</Step.Title>
+                <Step.Description>Projekt in Bearbeitung</Step.Description>
+              </Step.Content>
+            </Step>
+
+            <Step active>
+              <Icon name="flag checkered" />
+              <Step.Content>
+                <Step.Title>Abschluss</Step.Title>
+                <Step.Description>Projekt abgeschlossen</Step.Description>
+              </Step.Content>
+            </Step>
+          </Step.Group>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
 
-export default ProjectDetail
+export default ProjectDetail;
