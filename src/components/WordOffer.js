7851bdb6259398ff24a699111f00129228997050
rRakarare <rRene333@googlemail.com>
2021-05-25 07:57:39 +0200

    asdadsdas

--- src/components/WordOffer.js
+++ src/components/WordOffer.js
@@ -1,72 +1,84 @@
-import React, {useEffect} from "react";
-import { TemplateHandler } from 'easy-template-x';
+import React, { useEffect, useState } from "react";
+import { TemplateHandler } from "easy-template-x";
+var _ = require("lodash");
 
-const loadFile = async (url) => {
-  const response = await fetch(url)
-  const template = await response.blob()
-  return template
-}
+const WordOffer = ({ filepath, filename, data }) => {
+  const loadFile = async (url) => {
+    const response = await fetch(url);
+    const template = await response.blob();
+    return template;
+  };
 
-const loadImage = async (url) => {
-  const response = await fetch(url);
-  const blob = await response.blob()
-  console.log(blob)
-  return blob
-}
+  const getImage = async (url, width, height) => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return {
+      _type: "image",
+      source: blob,
+      format: blob.type,
+      width: width,
+      height: height,
+    };
+  };
 
+  function saveFile(filename, blob) {
+    const blobUrl = URL.createObjectURL(blob);
 
-function saveFile(filename, blob) {
+    let link = document.createElement("a");
+    link.download = filename;
+    link.href = blobUrl;
 
-  const blobUrl = URL.createObjectURL(blob);
+    document.body.appendChild(link);
+    link.click();
 
-  let link = document.createElement("a");
-  link.download = filename;
-  link.href = blobUrl;
-
-
-  document.body.appendChild(link);
-  link.click();
-
-
-  setTimeout(() => {
+    setTimeout(() => {
       link.remove();
       window.URL.revokeObjectURL(blobUrl);
       link = null;
-  }, 0);
-}
-
-
-const WordOffer = () => {
-
-  useEffect(() => {
-    loadImage("/logo512.png")
-  }, [])
-
-  const img = loadImage("/logo512.png");
-
-  const data = {
-    posts: [
-        { author: 'Alon Bar', text: 'Very important\ntext here!' },
-        { author: 'Alon Bar', text: 'Forgot to mention that...' }
-    ],
-    'iq': {
-      _type: "image",
-      source: img,
-      format: MimeType.Png,
-      width: 200,
-      height: 200
+    }, 0);
   }
 
-};
+  const datanew = async () => {
+    const allkeys = Object.keys(data);
+    allkeys.forEach((key) => {
+      if (_.isArray(data[key])) {
+        data[key].forEach((subobject, subindex) => {
+          const subkeys = Object.keys(subobject);
+          subkeys.forEach((subkey) => {
+            if (data[key][subindex][subkey].isImage) {
+              getImage(
+                data[key][subindex][subkey].url,
+                data[key][subindex][subkey].width,
+                data[key][subindex][subkey].height
+              ).then((res) => (data[key][subindex][subkey] = res));
+            }
+          });
+        });
+      } else {
+        if (data[key].isImage) {
+          getImage(data[key].url, data[key].width, data[key].height).then(
+            (res) => (data[key] = res)
+          );
+        }
+      }
+    });
+
+    console.log(data);
+
+    return data;
+  };
 
   const generateDocument = async () => {
-    const handler = new TemplateHandler()
-    const template = await loadFile('/tag-example.docx')
-    const doc = await handler.process(template, data)
-    saveFile('myfile.docx', doc)
-  }
+    const handler = new TemplateHandler();
+    const template = await loadFile(filepath);
+
+    const data = await datanew();
+
+    const doc = await handler.process(template, data);
+    saveFile(filename, doc);
+  };
 
   return <button onClick={() => generateDocument()}>Create Offer</button>;
-}
+};
 
 export default WordOffer;
--- src/screens/ProjectDetailScreen.js
+++ src/screens/ProjectDetailScreen.js
@@ -3,10 +3,25 @@ import { Grid, Segment, Step } from "semantic-ui-react";
 import ProjectInvoices from "../components/ProjectInvoices";
 import ProjectSteps from "../components/ProjectSteps";
 import ProjectDetail from "../components/ProjectDetail";
-import WordOffer from '../components/WordOffer'
+import WordOffer from "../components/WordOffer";
 
 function ProjectDetailScreen({ match }) {
   const projectID = match.params.id;
+
+  const dataold = {
+    loop: [
+      {
+        name: "rene",
+        image: { isImage: true, url: "/smile.png", width: 200, height: 200 },
+      },
+      {
+        name: "couti",
+        image: { isImage: true, url: "/logo512.png", width: 200, height: 200 },
+      },
+    ],
+    asdqwe: { isImage: true, url: "/logo512.png", width: 200, height: 200 },
+    qwe2: { isImage: true, url: "/smile.png", width: 200, height: 200 },
+  };
 
   return (
     <Grid stackable columns={3}>
@@ -14,14 +29,18 @@ function ProjectDetailScreen({ match }) {
         <Grid.Column computer={4} tablet={16}>
           <Segment>
             <ProjectDetail projectID={projectID} />
-            <WordOffer/>
+            <WordOffer
+              filepath="/test.docx"
+              filename="asdqwe.docx"
+              data={dataold}
+            />
           </Segment>
         </Grid.Column>
         <Grid.Column computer={4} tablet={16}>
-        <Segment>
-          <Step.Group fluid vertical>
-            <ProjectSteps projectID={projectID} />
-          </Step.Group>
+          <Segment>
+            <Step.Group fluid vertical>
+              <ProjectSteps projectID={projectID} />
+            </Step.Group>
           </Segment>
         </Grid.Column>
         <Grid.Column computer={8} tablet={16}>
